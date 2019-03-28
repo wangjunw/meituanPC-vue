90224c2197f49b9bbdb232a4229c7df3210773f2
junweiwang <junweiwang@uqiauto.com>
2019-03-28 19:58:56 +0800

    同步

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>登录</div>
+  <div>
+    <header>
+      <a href="http://www.meituan.com">美团网</a>
+    </header>
+  </div>
 </template>
 <script>
 export default {
   layout: "loginTemplate"
 };
 </script>
+<style lang="scss" scoped>
+@import "@/assets/css/login/login.scss";
+</style>
+
