a2b940db9505a4c9757df19ddb823c32d0cfa585
junwei <1157593964@qq.com>
2019-04-04 21:55:22 +0800

    重置密码页编写中

--- pages/account/findPassword.vue
+++ pages/account/findPassword.vue
@@ -13,18 +13,42 @@
       </ul>
       <div class="container">
         <div v-if="currentStep === 1" class="step1">
-          <p>请填写你要找回密码的美团账号</p>
-          <input type="text" class="account">
-          <el-button type="success" @click="nextHandler(2)">下一步</el-button>
+          <p class="desc">请填写你要找回密码的美团账号</p>
+          <input type="text" class="account" v-model="step1Params.account">
+          <el-button
+            type="success"
+            :disabled="step1Params.account == ''"
+            @click="confirmAccount"
+          >下一步</el-button>
         </div>
         <div v-else-if="currentStep === 2" class="step2">
-          <p>为了您的账户安全，请先验证手机</p>
+          <p class="desc">为了您的账户安全，请先验证邮箱</p>
+          <div class="tips" v-if="showtips">
+            <i class="el-icon-success successIcon" v-if="tipsType === success"></i>
+            <i class="el-icon-warning warningIcon" v-else></i>
+            {{tipsText}}
+          </div>
+          <el-form label-width="60px" :model="formParams">
+            <el-form-item label="邮箱">
+              <el-input v-model="step2Params.email"></el-input>
+            </el-form-item>
+            <el-form-item label="验证码">
+              <el-input v-model="step2Params.code"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button round @click="checkEmail">下一步</el-button>
+            </el-form-item>
+          </el-form>
         </div>
-        <div v-else-if="currentStep === 3" class="step3">重置密码</div>
+        <div v-else-if="currentStep === 3" class="step3">
+          <p class="desc">请为你的账号{{''}}设置一个新密码</p>
+          <input class="account" type="password" placeholder="请设置8-32位(数字+字母)">
+          <el-button type="success" @click="saveNewPass">保存新密码</el-button>
+        </div>
         <div v-else class="step4">
           <h2>恭喜您已经成功修改了登录密码</h2>
           <p>您的登录密码已经重新设置，请妥善保管</p>
-          <el-button type="success" @click="nextHandler(2)">立即登录</el-button>
+          <el-button type="success" @click="toLogin">立即登录</el-button>
         </div>
       </div>
     </main>
@@ -40,7 +64,17 @@ export default {
   layout: "findPassTemplate",
   data() {
     return {
-      currentStep: 1
+      step1Params: {
+        account: ""
+      },
+      step2Params: {
+        email: "",
+        code: ""
+      },
+      currentStep: 1,
+      tipsType: "success",
+      tipsText: "验证码以发送，请稍后",
+      showTips: true
     };
   },
   components: {
@@ -49,9 +83,12 @@ export default {
     loginFoot
   },
   methods: {
-    nextHandler(next) {
-      this.currentStep = next;
-    }
+    confirmAccount() {
+      this.currentStep = 2;
+    },
+    checkEmail() {},
+    saveNewPass() {},
+    toLogin() {}
   }
 };
 </script>
